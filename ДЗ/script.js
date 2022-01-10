464e4f0b95b51a1ddeb0a8bd7adb40d4beaf1089
KazancevaYuliya <87359439+KazancevaYuliya@users.noreply.github.com>
2022-01-10 12:41:23 +0300

    Add files via upload

---
+++ ДЗ/script.js
@@ -0,0 +1,70 @@
+// ---1.---
+
+//function getLocation() {
+//console.log(this.continent + " " + this.country)
+//}
+
+//let developer1 = {
+//firstName: 'Maria',
+//lastName: 'Y.',
+//country: 'Cyprus',
+//continent: 'Europe',
+//age: 30,
+//language: 'Java',
+//getLocation: getLocation
+//}
+
+//let developer2 = {
+//firstName: 'Victoria',
+//lastName: 'T.',
+//country: 'Puerto Rico',
+//continent: 'Americas',
+//age: 70,
+//language: 'Python',
+//getLocation: getLocation
+//}
+//console.log(developer1.getLocation())
+//console.log(developer1.getLocation())
+
+
+
+
+// ---3.---
+
+//const users = [
+    //{ name: 'Ivan', year: 1994 },
+    //{ name: 'Olga', year: 1998 },
+    //{ name: 'Vlad', year: 1988 },
+    //{ name: 'Vadim', year: 2002 },
+    //{ name: 'Ira', year: 1991 },
+    //{ name: 'Svetlana', year: 2004 },
+//]
+//const copy = []
+//for (let i = 0; i < users.length; i++) {
+    //copy.push(users[i])
+//}
+//users.forEach(function (users) {
+    //copy.push(users)
+//})
+//console.log(users)
+
+//users.map((obj) => {
+    //obj.age 
+    //obj['age'] 
+    //users[0].age = 28
+    //users[1].age = 24
+    //users[2].age = 34
+    //users[3].age = 20
+    //users[4].age = 31
+    //users[5].age = 18
+
+  //return obj
+    
+//})
+
+//const newUsers = users.filter(function(users) {
+    
+
+//return users.age > 25
+//})
+//console.log(newUsers)
